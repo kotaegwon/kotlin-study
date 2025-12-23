4cc4179c53580ed324dc69df262954a255f34af4
kotaegwon <rhxornjs@gmail.com>
2025-12-23 11:21:14 +0900

    ViewPager2

--- AndroidLab/ch11_jetpack/src/main/java/com/example/ch11_jetpack/MainActivity.kt
+++ AndroidLab/ch11_jetpack/src/main/java/com/example/ch11_jetpack/MainActivity.kt
@@ -12,20 +12,48 @@ import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.ch11_jetpack.databinding.ActivityMainBinding
 
+/**
+ * ViewPager2를 이용해 여러 Fragment를 스와이프 방식으로 전환하는 메인 액티비티.
+ *
+ * Toolbar, DrawerLayout, SearchView를 함께 사용하며
+ * ViewPager2를 통해 Fragment 간 좌우 전환을 제공한다.
+ */
 class MainActivity : AppCompatActivity() {
 
+    /**
+     * DrawerLayout과 Toolbar를 연결하는 토글 버튼
+     */
     lateinit var toggle: ActionBarDrawerToggle
 
-    class MyFragmentPagerAdapter(activity: FragmentActivity): FragmentStateAdapter(activity){
+    /**
+     * ViewPager2에서 사용할 FragmentStateAdapter
+     *
+     * 각 프레그먼트를 스와이프 방식으로 전환
+     */
+    class MyFragmentPagerAdapter(activity: FragmentActivity) : FragmentStateAdapter(activity) {
+
+        /**
+         * ViewPager2에 표시할 Fragment 목록
+         */
         val fragments: List<Fragment>
+
         init {
             fragments = listOf(OneFragment(), TwoFragment(), ThreeFragment())
         }
 
+        /**
+         * 전체 Fragment 개수 반환
+         */
         override fun getItemCount(): Int {
             return fragments.size
         }
 
+        /**
+         * position에 해당하는 Fragment를 생성
+         *
+         * @param position Fragment의 위치
+         * @return 해당 위치에 표시할 Fragment
+         */
         override fun createFragment(position: Int): Fragment {
             return fragments[position]
         }
@@ -38,8 +66,10 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbar)
 
-        toggle = ActionBarDrawerToggle(this, binding.main, R.string.drawer_opened,
-            R.string.drawer_colsed)
+        toggle = ActionBarDrawerToggle(
+            this, binding.main, R.string.drawer_opened,
+            R.string.drawer_colsed
+        )
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         toggle.syncState()
 
@@ -53,7 +83,7 @@ class MainActivity : AppCompatActivity() {
 
         val menuItem = menu?.findItem(R.id.menu_search)
         val searchView = menuItem?.actionView as SearchView
-        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextChange(newText: String?): Boolean {
                 return true
             }
@@ -67,7 +97,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if(toggle.onOptionsItemSelected(item)){
+        if (toggle.onOptionsItemSelected(item)) {
             return true
         }
         return super.onOptionsItemSelected(item)
